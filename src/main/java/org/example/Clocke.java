3dbc2837c6abe7eae60836ad2dfd7bcd991d1e8f
Shanika Wijerathna <shanika.wijerathna@countiesenergy.co.nz>
2023-03-23 07:10:29 +1300

    Initial commit

---
+++ src/main/java/org/example/Clocke.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Clocke {
+
+    public int calculate() {
+        return 1;
+    }
+}
---
+++ src/test/java/org/example/ClockeTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class ClockeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
